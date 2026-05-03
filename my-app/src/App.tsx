2d33587370a6d126c7bc9954de67b81914c3da24
mu <01-mu@users.noreply.github.com>
2026-05-03 13:51:14 +0900

    https://react.dev/learn

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -5,11 +5,28 @@ function MyButton() {
 }
 
 function App() {
+  type User = {
+    name: string;
+    imageUrl: string;
+    imageSize: number;
+  };
+
+  const user: User = {
+    name: "mu",
+    imageUrl: "https://placehold.jp/150x150.png",
+    imageSize: 90,
+  };
+
   return (
     <>
-      <h1>Welcome to my app</h1>
       <MyButton />
-      <img className="avatar" src="https://placehold.jp/150x150.png" alt="avatar" />
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt="avatar"
+        style={{ width: `${user.imageSize}px`, height: `${user.imageSize}px` }}
+      />
     </>
   );
 }
