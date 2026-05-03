32635b15336c6990454c40e63f44325c5110e4ad
mu <01-mu@users.noreply.github.com>
2026-05-03 13:32:35 +0900

    https://react.dev/learn

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,7 +1,17 @@
 import "./App.css";
 
+function MyButton() {
+  return <button>I'm a button</button>;
+}
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+      <img className="avatar" src="https://placehold.jp/150x150.png" alt="avatar" />
+    </>
+  );
 }
 
 export default App;
